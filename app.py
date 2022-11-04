7af10b32216677bb7d82abe3cdbdaf4c76b56203
mkcnp <45289974+mkcnp@users.noreply.github.com>
2022-11-04 11:42:29 +0530

    Fixed a small issue
    Contact numbers from Excel were read as floats by python. Made a small change so as to convert them to integers.

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import pywhatkit as pwk
 wb = load_workbook("contacts.xlsx")
 ws = wb.active
 
-for i in range(2,4):
-    contact_number = f"+{str(ws.cell(row=i, column=2).value)}"
+for i in range(2,10):
+    contact_number = f"+{int(ws.cell(row=i, column=2).value)}"
     contact_name = ws.cell(row=i, column=1).value
-    pwk.sendwhatmsg_instantly(str(contact_number),
-						f'''Hey {contact_name}! How's it going?''')
+    pwk.sendwhatmsg_instantly(contact_number,
+                        f'''Hey {contact_name}! How's it going?''')
     time.sleep(2)
     pyautogui.typewrite(["enter"])
-    time.sleep(2)  
+    time.sleep(2)
